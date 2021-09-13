8fa2fab7c696e9ac92c47d3abf71c95c16e1e98a
David Bradford <davebradford@knights.ucf.edu>
2021-09-13 15:52:59 -0400

    Initial commit

---
+++ src/main/java/org/example/CarIssues.java
@@ -0,0 +1,59 @@
+package org.example;
+
+/*
+ *  UCF COP3330 Fall 2021 Assignment 1 Solution
+ *  Copyright 2021 David Bradford
+ */
+
+import java.util.Scanner;
+
+public class CarIssues {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        char pick;
+
+        System.out.print("Is the car silent when you turn the key? ");
+        pick = input.next().charAt(0);
+        if (pick == 'y') {
+            System.out.print("Are the battery terminals corroded? ");
+            pick = input.next().charAt(0);
+            if (pick == 'y')
+                System.out.println("Clean terminals and try starting again.");
+            else
+                System.out.println("Replace cables and try again.");
+        } else {
+            System.out.print("Does the car make a slicking noise? ");
+            pick = input.next().charAt(0);
+            if (pick == 'y')
+                System.out.println("Replace the battery.");
+            else {
+                System.out.print("Does the car crank up but fail to start? ");
+                pick = input.next().charAt(0);
+                if (pick == 'y')
+                    System.out.print("Check spark plug connections.");
+                else {
+                    System.out.print("Does the engine start and then die? ");
+                    pick = input.next().charAt(0);
+
+                    if (pick == 'y') {
+
+                        System.out.print("Does you car have fuel injection? ");
+                        pick = input.next().charAt(0);
+
+                    if (pick == 'y')
+                        System.out.print("Get it in for service.");
+
+                    if (pick == 'n')
+                        System.out.print("Check to ensure the choke is opening and closing.");
+                }
+
+                    else {
+                        System.out.println("This should not be possible.");
+                    }
+            }
+        }
+    }
+}
+}
+
+
